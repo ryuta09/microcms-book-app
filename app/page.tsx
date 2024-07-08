482f30c64d7f6f8a189d3c20bb73b7a7914c8107
古賀龍太 <kogaryuuta@kogaryuutanoMac-mini.local>
2024-07-08 23:42:18 +0900

    feat: mvの実装

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,14 @@ import Image from "next/image";
 import styles from "./global.module.css";
 export default function Home() {
   return (
-    <div>
-      <h1 className={styles.title}>テクノロジーの力で世界を変える</h1>
-      <p className={styles.description}>
-        私たちは市場をリードしているグローバルテックカンパニーです。
-      </p>
-    </div>
+    <section className={styles.top}>
+      <div>
+        <h1 className={styles.title}>テクノロジーの力で世界を変える</h1>
+        <p className={styles.description}>
+          私たちは市場をリードしているグローバルテックカンパニーです。
+        </p>
+      </div>
+      <Image className={styles.bgImg} src={'/img-mv.jpg'} width={4000} height={1200} alt="" />
+    </section>
   );
 }
