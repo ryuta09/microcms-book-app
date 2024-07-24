c34df234d7e88df4c9197cbcee09de99d754decd
古賀龍太 <kogaryuuta@kogaryuutanoMac-mini.local>
2024-07-24 23:47:07 +0900

    feat: useSearchParamsを使用するとレンダリングが発生する度に中で使用されているコンポーネント全てがクライアントサイドになってしまうので修正

--- app/_components/SearchField/index.tsx
+++ app/_components/SearchField/index.tsx
@@ -1,9 +1,11 @@
 'use client'
 import Image from 'next/image'
 import styles from './index.module.css'
-import { useRouter } from 'next/navigation'
-export default function SearchField() {
+import { useRouter, useSearchParams } from 'next/navigation'
+import { Suspense } from 'react'
+function SearchFieldComponent() {
   const router = useRouter()
+  const searchParams = useSearchParams();
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     const q = e.currentTarget.elements.namedItem('q')
@@ -17,8 +19,16 @@ export default function SearchField() {
     <form onSubmit={handleSubmit} className={styles.form}>
       <label className={styles.search}>
         <Image src='/search.svg' alt='検索' width={16} height={16} />
-        <input type='text' name='q' placeholder='キーワードを入力' className={styles.searchInput}/>
+        <input type='text' name='q' placeholder='キーワードを入力' className={styles.searchInput} defaultValue={searchParams.get('q') ?? undefined}/>
       </label>
     </form>
+  )
+}
+
+export default function SearchField() {
+  return(
+    <Suspense>
+      <SearchFieldComponent/>
+    </Suspense>
   )
 }
