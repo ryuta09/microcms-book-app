e0467556c873fcd19a91ead2b1b8439e92883403
古賀龍太 <kogaryuuta@kogaryuutanoMac-mini.local>
2024-07-25 23:08:32 +0900

    feat: /news全てのファイルでrevalidateを適用させたいのでlayout.tsxに記述して配下のファイル全てに適用するように実装

--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -9,9 +9,7 @@ type Props = {
   searchParams: {
     dk?: string
   }
-}
-
-export const revalidate = 60                      ;
+}                    ;
 
 export default async function NewsDetail({params, searchParams}: Props) {
   const data = await getNewsDetail(params.slug, {
