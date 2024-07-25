fd13d147d2190a07e94428167cceaf38a1bf4c96
古賀龍太 <kogaryuuta@kogaryuutanoMac-mini.local>
2024-07-25 23:26:33 +0900

    feat: Basic認証及びmiddleware.tsファイルの削除

--- middleware.ts
+++
@@ -1,8 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-import { createNextAuthMiddleware } from "nextjs-basic-auth-middleware";
-
-export const middleware = createNextAuthMiddleware()
-
-export const config = {
-  matcher: ['/(.*)']
-}
