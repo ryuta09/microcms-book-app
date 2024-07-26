ab6fd64dc110ff7a63c52a52cb8cfc2a32720ec5
古賀龍太 <kogaryuuta@kogaryuutanoMac-mini.local>
2024-07-26 23:18:07 +0900

    feat: HubSpotを利用した送信のロジックを実装

--- app/_action/contact.tsx
+++ app/_action/contact.tsx
@@ -1,55 +1,107 @@
-'use server'
+"use server";
 function validateEmail(email: string) {
   const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  return pattern.test(email)
+  return pattern.test(email);
 }
 
-export default async function createContactDate(_prevState: any, formData: FormData) {
+export default async function createContactDate(
+  _prevState: any,
+  formData: FormData
+) {
   // formのname属性ごとに値を取り出す事ができる
   const rawFormData = {
-    lastname: formData.get('lastname') as string,
-    firstname: formData.get('firstname') as string,
-    company: formData.get('company') as string,
-    email: formData.get('email') as string,
-    message: formData.get('message') as string,
+    lastname: formData.get("lastname") as string,
+    firstname: formData.get("firstname") as string,
+    company: formData.get("company") as string,
+    email: formData.get("email") as string,
+    message: formData.get("message") as string,
+  };
+
+  if (!rawFormData.lastname) {
+    return {
+      status: "error",
+      message: "性を入力してください",
+    };
+  }
+  if (!rawFormData.firstname) {
+    return {
+      status: "error",
+      message: "名を入力してください",
+    };
+  }
+  if (!rawFormData.company) {
+    return {
+      status: "error",
+      message: "会社名を入力してください",
+    };
+  }
+  if (!rawFormData.email) {
+    return {
+      status: "error",
+      message: "メールアドレスの形式が誤ってます。",
+    };
+  }
+  if (!validateEmail(rawFormData.email)) {
+    return {
+      status: "error",
+      message: "メッセージを入力してください",
+    };
+  }
+  if (!rawFormData.message) {
+    return {
+      status: "error",
+      message: "メッセージを入力してください",
+    };
   }
 
-  if(!rawFormData.lastname) {
-    return {
-      status: "error",
-      message: "性を入力してください"
+  const result = await fetch(
+    `https://api.hsforms.com/submissions/v3/integration/submit/${process.env.HUBSPOT_PORTAL_ID}/${process.env.HUBSPOT_FORM_ID}`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        fields: [
+          {
+            objectTypeId: "0-1",
+            name: "lastname",
+            value: rawFormData.lastname,
+          },
+          {
+            objectTypeId: "0-1",
+            name: "firstname",
+            value: rawFormData.firstname,
+          },
+          {
+            objectTypeId: "0-1",
+            name: "company",
+            value: rawFormData.company,
+          },
+          {
+            objectTypeId: "0-1",
+            name: "email",
+            value: rawFormData.email,
+          },
+          {
+            objectTypeId: "0-1",
+            name: "message",
+            value: rawFormData.message,
+          },
+        ],
+      }),
     }
-  }
-  if(!rawFormData.firstname) {
-    return {
-      status: "error",
-      message: "名を入力してください"
-    }
-  }
-  if(!rawFormData.company) {
-    return {
-      status: "error",
-      message: "会社名を入力してください"
-    }
-  }
-  if(!rawFormData.email) {
-    return {
-      status: "error",
-      message: "メールアドレスの形式が誤ってます。"
-    }
-  }
-  if(!validateEmail(rawFormData.email)) {
-    return {
-      status: "error",
-      message: "メッセージを入力してください"
-    }
-  }
-  if(!rawFormData.message) {
-    return {
-      status: "error",
-      message: "メッセージを入力してください"
+  );
+
+  try {
+    await result.json()
+  } catch(e){
+    console.log(e);
+    return{
+    status: 'error',
+    message: 'お問い合わせに失敗しました。'
     }
   }
 
-  return {status: "success", message: 'OK'}
+  return { status: "success", message: "OK" };
 }
