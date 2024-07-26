3762e149cd8b0f3f8675865cf11e0b7582042256
古賀龍太 <kogaryuuta@kogaryuutanoMac-mini.local>
2024-07-26 22:22:44 +0900

    feat: contactのコンポーネントとルーティング用の/contactを作成

---
+++ app/_components/ContactForm/index.tsx
@@ -0,0 +1,32 @@
+import styles from './index.module.css'
+export default function ContactForm() {
+  return(
+    <form className={styles.form}>
+      <div className={styles.horizontal}>
+        <div className={styles.item}>
+          <label className={styles.label} htmlFor="lastname">性</label>
+          <input type="text" id='lastname' name='lastname' className={styles.textfield} />
+        </div>
+        <div className={styles.item}>
+          <label className={styles.label} htmlFor="firstname">名</label>
+          <input type="text" id='firstname' name='firstname' className={styles.textfield} />
+        </div>
+        <div className={styles.item}>
+          <label className={styles.label} htmlFor="company">会社名</label>
+          <input type="text" id='company' name='company' className={styles.textfield} />
+        </div>
+        <div className={styles.item}>
+          <label className={styles.label} htmlFor="email">メールアドレス</label>
+          <input type="text" id='email' name='email' className={styles.textfield} />
+        </div>
+        <div className={styles.item}>
+          <label className={styles.label} htmlFor="message">メッセージ</label>
+          <textarea id='message' name='message' className={styles.textfield} />
+        </div>
+        <div className={styles.actions}>
+          <input type="submit" className={styles.button} value='送信する'/>
+        </div>
+      </div>
+    </form>
+  )
+}
