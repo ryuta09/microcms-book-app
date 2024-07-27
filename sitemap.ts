989bc2232c6e262dd32037799e23fa04766b7ab7
古賀龍太 <kogaryuuta@kogaryuutanoMac-mini.local>
2024-07-27 11:36:46 +0900

    feat: ニュースやカテゴリーのサイトマップを適用するための記述を追加

--- sitemap.ts
+++ sitemap.ts
@@ -1,8 +1,24 @@
 import { MetadataRoute } from "next";
+import { getAllCategory, getAllNewsList } from "./lib/microcms";
 
 const buildUrl = (path?: string) => `http://localhost:3000${path ?? ""}`;
 
-export default async function sitemap():Promise<MetadataRoute.Sitemap> {
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const newsContents = await getAllNewsList();
+  const categoryContents = await getAllCategory();
+
+  const newsUrls: MetadataRoute.Sitemap = newsContents.map((content) => ({
+    url: buildUrl(`/news/${content.id}`),
+    lastModified: content.revisedAt
+  }));
+
+  const categoryUrls: MetadataRoute.Sitemap = categoryContents.map((content) => ({
+    url: buildUrl(`/news/${content.id}`),
+    lastModified: content.revisedAt
+  }));
+
+
+
   const now = new Date();
 
   return [
@@ -11,16 +27,18 @@ export default async function sitemap():Promise<MetadataRoute.Sitemap> {
       lastModified: now,
     },
     {
-      url: buildUrl('/members'),
-      lastModified: now
+      url: buildUrl("/members"),
+      lastModified: now,
     },
     {
-      url: buildUrl('/contact'),
-      lastModified: now
+      url: buildUrl("/contact"),
+      lastModified: now,
     },
     {
-      url: buildUrl('/news'),
-      lastModified: now
-    }
-  ]
+      url: buildUrl("/news"),
+      lastModified: now,
+    },
+    ...newsUrls,
+    ...categoryUrls
+  ];
 }
